eb1c2ab003c1447190e5de1d6839dc267c8a2a2a
Caualu <caualuizsilvacorrea@gmail.com>
2024-04-08 19:27:05 -0300

    ex4

--- aula.5/exercicio4.py
+++ aula.5/exercicio4.py
@@ -1,7 +1,7 @@
 idade = int(input("digite a sua idade"))
 if idade < 16:
     print("voce ainda nao esta eligivel")
-elif <= 18 idade < 70:
+elif  18  <= idade < 70:
     print ("voce podera votar")
 else:
     print("voce esta eligivel para votar, mas nao é obrigatório")    
